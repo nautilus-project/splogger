41277c8efd5ccc7a8707dc6c96da63a0aad2efa0
jpcaulfi <joe.caulfield@solana.org>
2023-05-01 01:22:49 -0500

    termcolor

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use solana_program::msg;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 pub enum Splog {
     DEBUG,
@@ -8,43 +9,81 @@ pub enum Splog {
     CRITICAL,
 }
 
+/// Solana program logs.
 pub fn splog_log(splog: Splog, message: &str) {
-    match splog {
-        Splog::DEBUG => msg!("[DEBUG]: {}", message),
-        Splog::INFO => msg!("[INFO]: {}", message),
-        Splog::WARN => msg!("[WARN]: {}", message),
-        Splog::ERROR => msg!("[ERROR]: {}", message),
-        Splog::CRITICAL => msg!("[CRITICAL]: {}", message),
-    }
+    SploggerTerminal::print(splog, message);
 }
 
+/// General-use macro for Splogging.
 #[macro_export]
 macro_rules! splog {
     ($log:expr, $msg:expr) => {$crate::splog_log($log, $msg)};
     ($log:expr, $($arg:tt)*) => ($crate::splog_log($log, &format!($($arg)*)));
 }
 
+/// Log a debug message.
 #[macro_export]
 macro_rules! splog_debug {
     ($($arg:tt)*) => ($crate::splog!(Splog::DEBUG, &format!($($arg)*)));
 }
 
+/// Log an info message.
 #[macro_export]
 macro_rules! splog_info {
     ($($arg:tt)*) => ($crate::splog!(Splog::INFO, &format!($($arg)*)));
 }
 
+/// Log a warn message.
 #[macro_export]
 macro_rules! splog_warn {
     ($($arg:tt)*) => ($crate::splog!(Splog::WARN, &format!($($arg)*)));
 }
 
+/// Log an error message.
 #[macro_export]
 macro_rules! splog_error {
     ($($arg:tt)*) => ($crate::splog!(Splog::ERROR, &format!($($arg)*)));
 }
 
+/// Log a critical message.
 #[macro_export]
 macro_rules! splog_critical {
     ($($arg:tt)*) => ($crate::splog!(Splog::CRITICAL, &format!($($arg)*)));
+}
+
+/// Terminal color controls.
+pub struct SploggerTerminal {}
+
+impl SploggerTerminal {
+    /// Initalize control over the terminal color & output.
+    pub fn print(splog: Splog, message: &str) {
+        let mut stdout = StandardStream::stdout(ColorChoice::Always);
+        let mut colspec = ColorSpec::new();
+        match splog {
+            Splog::DEBUG => {
+                msg!("[DEBUG]: {}", message);
+            }
+            Splog::INFO => {
+                colspec.set_fg(Some(Color::Cyan)).set_bold(true);
+                stdout.set_color(&colspec).unwrap();
+                msg!("[INFO]: {}", message);
+            }
+            Splog::WARN => {
+                colspec.set_fg(Some(Color::Yellow)).set_bold(true);
+                stdout.set_color(&colspec).unwrap();
+                msg!("[WARN]: {}", message);
+            }
+            Splog::ERROR => {
+                colspec.set_fg(Some(Color::Red)).set_bold(true);
+                stdout.set_color(&colspec).unwrap();
+                msg!("[ERROR]: {}", message);
+            }
+            Splog::CRITICAL => {
+                colspec.set_fg(Some(Color::Red)).set_bold(true);
+                stdout.set_color(&colspec).unwrap();
+                msg!("[CRITICAL]: {}", message);
+            }
+        };
+        stdout.reset().unwrap();
+    }
 }
